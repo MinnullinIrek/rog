d5bb6f95627d307fc4c52a72b26ca9e2604ebb22
MinnullinIrek <irekminn4@gmail.com>
2016-05-08 14:09:07 +0300

    первый коммит

---
+++ ConsoleApplication1/IBonus.java
@@ -0,0 +1,20 @@
+package ConsoleApplication1;
+
+/**
+ * @author admin
+ * @version 1.0
+ * @created 26-апр-2016 22:01:16
+ */
+public class IBonus {
+
+	public CTime m_CTime;
+
+	public IBonus(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+}
